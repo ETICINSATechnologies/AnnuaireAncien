a7dffd62f4720b38dd1cde0474dfa54547f5dcfd
Marah <marah.galy@gmail.com>
2018-05-10 23:15:45 +0200

    Page de connexion

--- front/src/connexion.php
+++ front/src/connexion.php
@@ -2,15 +2,34 @@
 <html lang="en">
 <head>
     <?php include 'head.php'?>
-    <link rel="stylesheet" href="../style/ordinateur/accueil.css">
+    <link rel="stylesheet" href="../style/ordinateur/connexion.css">
+    <script src="../script/connexion.js"></script>
 </head>
 
 <body>
 <?php include 'header.php'; ?>
 <?php include 'nav.php'; ?>
 
-<section>
-</section>
+
+    <form id="connect_form">
+
+        <div id="response_area">
+
+        </div>
+
+            <p> <b>Identifiant </b> </p>
+            <input type="text" id="email">
+            <p> <b>Mot de passe </b> </p>
+            <input type="text" id="password">
+            <br> <br>
+            <a href="aide.php"> Mot de passe oublié ?</a>
+            <br> <br>
+            <input id="connect_input" type="button" value="Se connecter" onclick="connect()">
+
+
+
+    </form>
+
 
 <?php include 'footer.php'; ?>
 <?php include '../script/config.php'; ?>

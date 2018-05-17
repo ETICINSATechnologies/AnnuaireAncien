f98e241123c03256c68f71de02fe3b7f213a8fb8
Marah <marah.galy@gmail.com>
2018-05-17 16:29:26 +0200

    Admin

--- front/src/ajout.php
+++ front/src/ajout.php
@@ -1,12 +1,82 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include 'head.php'?>
+<head>
+    <?php include 'head.php' ?>
+    <link rel="stylesheet" href="../style/ordinateur/profil.css">
+</head>
 
-<body>
+<body onload="init()">
 <?php include 'header.php'; ?>
 <?php include 'nav.php'; ?>
+<?php include '../../services/updatePicture.php'; ?>
+
+<section id="form_area">
+
+    <form id="profil_form">
+        <div id="image"> </div>
+
+        <p id="p_lastname" class="right p_info"> Nom </p>
+
+        <input type="text" id="lastname" class="right" value="" title="lastname">
+
+        <p id="p_firstname" class="right p_info"> Prénom </p>
+
+        <input type="text" id="firstname" class="right" value="" title="firstname">
+
+        <p id="p_phone" class="p_info"> Téléphone </p>
+
+        <p id="p_department" class="p_info"> Département </p>
+
+        <input type="text" id="phone" value="" title="phone">
+
+        <input type="text" id="department" value="" title="department">
+
+        <p id="p_email" class="p_info"> Adresse mail </p>
+
+        <p id="p_etic_position" class="p_info"> Poste che Etic </p>
+
+        <input type="text" id="email" value="" title="email">
+
+        <input type="text" id="etic_position" value="" title="etic_position">
+
+        <p id="p_company" class="p_info"> Travaille chez </p>
+
+        <p id="p_mandate_year" class="p_info"> Année à Etic </p>
+
+        <input type="text" id="company" value="" title="company">
+
+        <input type="text" id="mandate_year" value="" title="mandate_year">
+
+        <div id="info_area"> </div>
+
+        <div id="button" class="right">
+            <input id="connect_input" type="button" value="Sauvegarder" onclick="update()">
+        </div>
+    </form>
+
+    <div id="fenetre">
+        <form id="image_form" method="post" action="../../services/updatePicture.php" enctype="multipart/form-data">
+            <div id="image2"> </div>
+            <input type="file" name="icone" id="icone" /><br/>
+            <input id="precedent" type="button" value="précedent">
+            <input id="savefile" type="submit" name="submit" value="suivant"/>
+        </form>
+    </div>
+</section>
+<script>
+    document.getElementById('image').addEventListener('click', function() {
+        document.getElementById('fenetre').style.display = 'block';
+    });
+    document.getElementById('precedent').addEventListener('click', function() {
+        document.getElementById('fenetre').style.display = 'none';
+    });
+</script>
+
+
 
 <?php include 'footer.php'; ?>
 <?php include '../script/config.php'; ?>
+<script src="../script/update.js"></script>
+<script src="../script/init.js"></script>
 </body>
 </html>
--- services/update.php
+++ services/update.php
@@ -12,45 +12,88 @@ $values = array_values($method);
 
 if (validateRequest($validAttributes, $attributes) && isset($_SESSION['id']))
 {
-    $sql = 'UPDATE membres';
-
-    for ($i = 0; $i < $parametersNb; $i++)
-    {
-        if ($i == 0)
+    if($_SESSION['admin']=="true"){
+        echo "ici";
+        $sql = 'INSERT INTO membres';
+        $sql .='(';
+        for ($i = 0; $i < $parametersNb; $i++)
         {
-            $sql .= ' SET ';
-            $sql .= $attributes[$i] . ' = ' . ':value' . $i;
+            if($i==$parametersNb-1){
+                $sql .=$attributes[$i] .')';
+            }
+            else{
+                $sql .=$attributes[$i] .',';
+            }
+        }
+        $sql .='VALUES(';
+        for ($i = 0; $i < $parametersNb; $i++)
+        {
+            if($i==$parametersNb-1){
+                $sql .='?' .')';
+            }
+            else{
+                $sql .='?' .',';
+            }
+
+        }
+
+        echo $sql;
+        $stmt = $bdd->prepare($sql);
+        for ($i = 1; $i <= $parametersNb; $i++)
+        {
+            $stmt->bindParam( $i, $values[$i-1]);
+        }
+        $data = $stmt->execute();
+        echo $data;
+
+
+    }
+    else{
+
+        $sql = 'UPDATE membres';
+
+        for ($i = 0; $i < $parametersNb; $i++)
+        {
+            if ($i == 0)
+            {
+                $sql .= ' SET ';
+                $sql .= $attributes[$i] . ' = ' . ':value' . $i;
+            }
+            else
+            {
+                $sql .= ',';
+                $sql .= $attributes[$i] . ' = ' . ':value' . $i;
+            }
+        }
+
+        $sql .= ' WHERE ';
+        $sql .= 'id' . ' = ' . $_SESSION['id'];
+
+        echo $sql;
+
+        $stmt = $bdd->prepare($sql);
+
+        for ($i = 0; $i < $parametersNb; $i++)
+        {
+            $stmt->bindParam(':value' . $i, $values[$i]);
+        }
+        $data = $stmt->execute();
+        echo $data;
+
+        if (!$stmt)
+        {
+            die ('error because ' . print_r($bdd->errorInfo(), true));
         }
         else
         {
-            $sql .= ',';
-            $sql .= $attributes[$i] . ' = ' . ':value' . $i;
+            echo $data;
         }
     }
 
-    $sql .= ' WHERE ';
-    $sql .= 'id' . ' = ' . $_SESSION['id'];
-
-    echo $sql;
-
-    $stmt = $bdd->prepare($sql);
-
-    for ($i = 0; $i < $parametersNb; $i++)
-    {
-        $stmt->bindParam(':value' . $i, $values[$i]);
-    }
-
-    $data = $stmt->execute();
 
 
-    if (!$stmt)
-    {
-        die ('error because ' . print_r($bdd->errorInfo(), true));
-    }
-    else
-    {
-        echo $data;
-    }
+
+
 }
 
 

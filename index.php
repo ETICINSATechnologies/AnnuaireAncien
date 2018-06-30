f358ba1fa4ae7f2075f9ca08f739dcf553e1f0cb
laurenttainturier <laurent.tainturier.pro@gmail.com>
2018-06-30 18:15:40 +0200

    fix bug index.php

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>ETIC INSA Technologies</title>
-</head>
-<body>
-<script>
-    window.location = "front/src/accueil.php";
-</script>
-</body>
+<?php header('Location: front/src/accueil.php');
